b09346c74313822bff29cb6391ddac6418f58b02
Sinem Güngör <sinemgngr@outlook.com>
2024-05-15 17:09:05 +0300

    yönetici panel logout

---
+++ MVC_Project_Group_4/Areas/YoneticiPaneli/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Project_Group_4.Models.Concrete;
+
+namespace MVC_Project_Group_4.Areas.YoneticiPaneli.Controllers
+{
+    [Area("YoneticiPaneli")]
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<Uye> _signInManager;
+        private readonly UserManager<Uye> _userManager;
+
+        public LoginController(SignInManager<Uye> signInManager, UserManager<Uye> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            //return RedirectToAction("Index", "Home");
+            return Redirect("~/Home/Index");
+            //return LocalRedirect("~/localhost:5168/Home/Index");
+        }
+    }
+}
