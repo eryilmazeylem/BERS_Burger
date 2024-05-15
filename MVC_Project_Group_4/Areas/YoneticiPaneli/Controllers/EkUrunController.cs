6f5350749caf15c3867f626ac08fc23c91e050ad
Sinem Güngör <sinemgngr@outlook.com>
2024-05-15 12:55:40 +0300

    ek ürün güncelle

--- MVC_Project_Group_4/Areas/YoneticiPaneli/Controllers/EkUrunController.cs
+++ MVC_Project_Group_4/Areas/YoneticiPaneli/Controllers/EkUrunController.cs
@@ -20,14 +20,14 @@ namespace MVC_Project_Group_4.Areas.YoneticiPaneli.Controllers
             this.db = db;
         }
 
-        EkstraMalzemeVM vm=new EkstraMalzemeVM();
+        EkstraMalzemeVM vm = new EkstraMalzemeVM();
 
         public IActionResult EkstraMalzemeListele()
         {
-            vm.EkstraMalzemeler=db.EkstraMalzemeler.ToList();
+            vm.EkstraMalzemeler = db.EkstraMalzemeler.ToList();
             return View(vm);
         }
-       
+
         public IActionResult EkstraMalzemeEkle()
         {
             return View(vm.EkstraMalzemeEkleVM);
@@ -36,13 +36,13 @@ namespace MVC_Project_Group_4.Areas.YoneticiPaneli.Controllers
         [HttpPost]
         public IActionResult EkstraMalzemeEkle(EkstraMalzemeEkleVM ekstra)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                EkstraMalzeme ekstraMalzeme=new EkstraMalzeme();
+                EkstraMalzeme ekstraMalzeme = new EkstraMalzeme();
                 ekstraMalzeme.Ad = ekstra.Ad;
-                ekstraMalzeme.Fiyat=ekstra.Fiyat;
-                ekstraMalzeme.Aciklama=ekstra.Aciklama;
-                ekstraMalzeme.Adet=ekstra.Adet;
+                ekstraMalzeme.Fiyat = ekstra.Fiyat;
+                ekstraMalzeme.Aciklama = ekstra.Aciklama;
+                ekstraMalzeme.Adet = ekstra.Adet;
 
                 Guid guid = Guid.NewGuid();
                 string dosyaAdi = guid.ToString();
@@ -63,7 +63,7 @@ namespace MVC_Project_Group_4.Areas.YoneticiPaneli.Controllers
         }
         public IActionResult EkstraMalzemeSil(int id)
         {
-            var ekstraMalzeme= db.EkstraMalzemeler.FirstOrDefault(x=>x.EkstraMalzemeID==id);
+            var ekstraMalzeme = db.EkstraMalzemeler.FirstOrDefault(x => x.EkstraMalzemeID == id);
 
             FileInfo fi = new FileInfo("wwwroot/Pictures/Ek_Urunler/" + ekstraMalzeme.PicturePath);
             fi.Delete();
@@ -85,6 +85,71 @@ namespace MVC_Project_Group_4.Areas.YoneticiPaneli.Controllers
             }
 
             return RedirectToAction(nameof(EkstraMalzemeListele));
+        }
+
+        public IActionResult EkstraMalzemeGuncelle(int id)
+        {
+            EkstraMalzemeEkleVM vm = db.EkstraMalzemeler.Where(x => x.EkstraMalzemeID.Equals(id)).Select(x => new EkstraMalzemeEkleVM()
+            {
+                Aciklama = x.Aciklama,
+                Ad = x.Ad,
+                Adet = x.Adet,
+                Fiyat = x.Fiyat,
+            }).FirstOrDefault();
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult EkstraMalzemeGuncelle(EkstraMalzemeEkleVM ekstraMalzeme, int id)
+        {
+            EkstraMalzeme ekstra = db.EkstraMalzemeler.FirstOrDefault(x => x.EkstraMalzemeID.Equals(id));
+
+            ekstra.Ad = ekstraMalzeme.Ad;
+            ekstra.Adet = ekstraMalzeme.Adet;
+            ekstra.Aciklama = ekstraMalzeme.Aciklama;
+            ekstra.Fiyat = ekstraMalzeme.Fiyat;
+
+            FileInfo fi = new FileInfo("wwwroot/Pictures/Ek_Urunler/" + ekstra.PicturePath);
+            fi.Delete();
+
+            Guid guid = Guid.NewGuid();
+
+            string dosyaAdi = guid.ToString();
+
+            dosyaAdi += ekstraMalzeme.PicturePath.FileName;
+
+            string dosyaYolu = "wwwRoot/Pictures/Ek_Urunler/";
+
+            ekstra.PicturePath = dosyaAdi;
+
+            FileStream fs = new FileStream(dosyaYolu + dosyaAdi, FileMode.Create);
+
+            ekstraMalzeme.PicturePath.CopyTo(fs);
+
+            fs.Close();
+
+            try
+            {
+                if (db.SaveChanges() > 0)
+                {
+                    TempData["infoguncelle"] = "Ek ürün başarıyla güncellendi.";
+                    db.SaveChanges();
+                    return RedirectToAction(nameof(EkstraMalzemeListele));
+                }
+            }
+            catch (Exception)
+            {
+
+                TempData["infoguncelle"] = "Ek ürün güncellenirken hata oluştu!";
+            }
+
+            return RedirectToAction(nameof(EkstraMalzemeListele));
+
+
+
+
+
         }
     }
 }
