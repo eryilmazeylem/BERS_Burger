922b6ededb8960e53b96513fa2938685de02c911
Sinem Güngör <sinemgngr@outlook.com>
2024-05-16 19:37:24 +0300

    partial deneme

--- MVC_Project_Group_4/Controllers/HomeController.cs
+++ MVC_Project_Group_4/Controllers/HomeController.cs
@@ -28,5 +28,8 @@ namespace MVC_Project_Group_4.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+     
+
     }
 }
